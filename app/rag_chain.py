22449408f7daeb91f3eebd712e2793746a3e45d4
buildwebpro <buildwebpro@gmail.com>
2025-04-27 13:13:15 +0700

    add folder update code & requirements

--- app/rag_chain.py
+++ app/rag_chain.py
@@ -1,6 +1,6 @@
 import os
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain.chains import RetrievalQA
 
 def get_rag_chain():
@@ -8,8 +8,9 @@ def get_rag_chain():
         openai_api_key=os.getenv("OPENROUTER_API_KEY"),
         openai_api_base="https://openrouter.ai/api/v1",
     )
+    db_path = os.path.join(os.getcwd(), "faiss_index")
     db = FAISS.load_local(
-        folder_path="faiss_index",
+        folder_path=db_path,
         embeddings=embeddings,
         allow_dangerous_deserialization=True
     )
